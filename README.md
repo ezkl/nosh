5f7b153e3e1114a882c5a63effea09513b7bc343
Ezekiel Templin <ezkl@me.com>
2011-06-30 23:32:50 -0400

    Basic structure in place.

---
+++ README.md
@@ -0,0 +1,29 @@
+nosh
+====
+
+Description
+-----------
+
+A very simple Typhoeus/Nokogiri wrapper. Conceptually based on the eat library.
+
+Usage
+-----
+
+    require 'nosh'
+
+Requirements
+------------
+* Typhoeus (v0.2.4)
+* Nokogiri (v1.4.6)
+
+Install
+-------
+
+    $ gem install nosh
+
+Copyright
+---------
+
+Copyright (c) 2011 Ezekiel Templin
+
+See LICENSE.txt for details.
--- lib/nosh.rb
+++ lib/nosh.rb
@@ -1,1 +1,22 @@
-require 'nosh/version'
+require 'typhoeus'
+require 'nokogiri'
+require 'singleton'
+
+require_relative 'nosh/version'
+
+module Nosh
+  module ObjectExtensions
+    def nosh(url, options = {})
+      parse = options[:parse] || false
+      
+      if (response = ::Typhoeus::Request.get(url)).success?
+        parse ? ::Nokogiri.parse(response.body) : response.body
+      else
+        puts "[#{response.code}]: \"#{response.status_message}\""
+        nil
+      end
+    end
+  end
+end
+
+::Object.send(:include, ::Nosh::ObjectExtensions) unless ::Object.method_defined?(:nosh)
